2eb9ca07116ca280f89bfd332847cf2e6c34ab5e
AL AMIN KOUSER <alaminkouser@icloud.com>
2023-01-18 19:29:22 +0600

    added: webRequest

--- manifest/background/service_worker/index.js
+++ manifest/background/service_worker/index.js
@@ -1,1 +1,19 @@
-console.log(true);
+[
+    "onBeforeRequest",
+    "onBeforeSendHeaders",
+    "onSendHeaders",
+    "onHeadersReceived",
+    "onAuthRequired",
+    "onBeforeRedirect",
+    "onResponseStarted",
+    "onCompleted",
+    "onErrorOccurred"
+].forEach(event => {
+    chrome.webRequest[event].addListener(
+        function (details) {
+            details.event = event;
+            console.log(details);
+        },
+        { urls: ["<all_urls>"] }
+    );
+});
