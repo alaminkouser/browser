4c62473ed9029b18292ebaaf2a69ee4d83076782
AL AMIN KOUSER <alaminkouser@icloud.com>
2023-03-07 12:47:31 +0600

    added: chrome action text and color

--- manifest/background/service_worker/index.js
+++ manifest/background/service_worker/index.js
@@ -12,6 +12,19 @@
     chrome.webRequest[event].addListener(
         function (details) {
             details.event = event;
+            chrome.tabs.query({ active: true, currentWindow: true }, function (currentTab) {
+                chrome.action.setBadgeBackgroundColor(
+                    { color: "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0") },
+                    () => { },
+                );
+                chrome.action.setBadgeText(
+                    {
+                        text: details["requestId"],
+                        tabId: currentTab[0]["id"]
+                    },
+                    () => { }
+                );
+            });
             console.log(details);
         },
         { urls: ["<all_urls>"] }
