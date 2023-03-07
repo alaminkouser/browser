ee69860a27522a252a9a90d16898fd6405ea4dfb
AL AMIN KOUSER <alaminkouser@icloud.com>
2023-03-07 14:12:32 +0600

    added: dictionary

---
+++ manifest/background/service_worker/root/webRequest.js
@@ -0,0 +1,32 @@
+[
+    "onBeforeRequest",
+    "onBeforeSendHeaders",
+    "onSendHeaders",
+    "onHeadersReceived",
+    "onAuthRequired",
+    "onBeforeRedirect",
+    "onResponseStarted",
+    "onCompleted",
+    "onErrorOccurred"
+].forEach(event => {
+    chrome.webRequest[event].addListener(
+        function (details) {
+            details.event = event;
+            chrome.tabs.query({ active: true, currentWindow: true }, function (currentTab) {
+                chrome.action.setBadgeBackgroundColor(
+                    { color: "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0") },
+                    () => { },
+                );
+                chrome.action.setBadgeText(
+                    {
+                        text: details["requestId"],
+                        tabId: currentTab[0]["id"]
+                    },
+                    () => { }
+                );
+            });
+            console.log(details);
+        },
+        { urls: ["<all_urls>"] }
+    );
+});
