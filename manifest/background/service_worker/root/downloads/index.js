db027145a2623bca0e5f512dd4f4d58f6b720250
alaminkouser <alaminkouser@icloud.com>
2023-05-03 22:44:19 +0600

    added: add downloads to aria2c

--- manifest/background/service_worker/root/downloads/index.js
+++ manifest/background/service_worker/root/downloads/index.js
@@ -6,6 +6,16 @@ chrome.downloads.onCreated.addListener((downloadItem) => {
             "method": "aria2.addUri",
             "params": ["token:" + PRIVATE.aria2c.secretToken, [downloadItem["finalUrl"]]]
         })
+    }).then((_) => {
+        fetch("http://" + PRIVATE.aria2c.host + ":" + PRIVATE.aria2c.port + "/jsonrpc", {
+            "method": "POST",
+            "body": JSON.stringify({
+                "method": "aria2.saveSession",
+                "params": ["token:" + PRIVATE.aria2c.secretToken]
+            })
+        }).then((_) => {
+            chrome.downloads.erase({});
+        });
     });
 });
 
