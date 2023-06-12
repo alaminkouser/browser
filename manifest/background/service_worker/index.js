715f0255af0daf1c88a2047d9c893fed240fa822
alaminkouser <alaminkouser@icloud.com>
2023-06-12 22:02:34 +0600

    removed: downloads

--- manifest/background/service_worker/index.js
+++ manifest/background/service_worker/index.js
@@ -3,7 +3,5 @@ importScripts("/private/index.js");
 importScripts("/lib/telegram/index.js");
 
 importScripts("./root/contextMenus/index.js");
-importScripts("./root/downloads/index.js");
 importScripts("./root/history/index.js");
-importScripts("./root/action.js");
 importScripts("./root/webRequest.js");
