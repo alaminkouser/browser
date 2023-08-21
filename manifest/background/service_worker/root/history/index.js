9fc465f1cf9827e9259c30a291a4dcc415b80716
AL AMIN KOUSER <alaminkouser@icloud.com>
2023-08-21 16:43:15 +0600

    updated: history

--- manifest/background/service_worker/root/history/index.js
+++ manifest/background/service_worker/root/history/index.js
@@ -1,13 +1,3 @@
 chrome.history.onVisited.addListener(function (value) {
-    fetch(PRIVATE.insert + "?" + PRIVATE.chromeHistoryGoogleSheetID + "=0", {
-        "method": "POST",
-        "body": JSON.stringify([
-            value.id,
-            value.lastVisitTime,
-            value.title,
-            value.typedCount,
-            value.url,
-            value.visitCount
-        ])
-    })
+    console.log(value);
 });
