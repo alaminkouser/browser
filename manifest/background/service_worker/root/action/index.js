11add3bd2b6aad44805df0f4aeb4b528c13e17fc
AL AMIN KOUSER <alaminkouser@icloud.com>
2023-08-21 18:07:57 +0600

    added: windows

---
+++ manifest/background/service_worker/root/action/index.js
@@ -0,0 +1,8 @@
+chrome.action.onClicked.addListener((_) => {
+    chrome.windows.create({
+        "url": "/manifest/background/service_worker/root/windows/index.html",
+        "type": "panel",
+        "height": 200,
+        "width": 400
+    });
+});
