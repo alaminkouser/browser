c41728a70891ed79d96f27d16ae6d1ac296cfee9
AL AMIN KOUSER <alaminkouser@icloud.com>
2023-09-02 01:11:43 +0600

    added: Upload Files

--- manifest/background/service_worker/root/windows/upload/index.js
+++ manifest/background/service_worker/root/windows/upload/index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "/lib/firebase/firebase-app.js";
 import { getAuth, onAuthStateChanged, signInAnonymously } from "/lib/firebase/firebase-auth.js";
-import { getStorage, ref, uploadBytes } from "/lib/firebase/firebase-storage.js";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "/lib/firebase/firebase-storage.js";
 
 let USER = null;
 const app = initializeApp(PRIVATE.firebaseConfig);
@@ -29,7 +29,24 @@ function signIn() {
 window.document.getElementById("file").addEventListener("change", (event) => {
     const file = event.target.files[0];
     const storageRef = ref(storage, USER.uid + "/" + file.name);
-    uploadBytes(storageRef, file).then((snapshot) => {
-        console.log('Uploaded a blob or file!');
-      });
-})
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on("state_changed",
+        (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            
+            window.document.getElementsByTagName("progress")[0].value = snapshot.bytesTransferred;
+            window.document.getElementsByTagName("progress")[0].max = snapshot.totalBytes;
+        },
+        (error) => {
+            // Handle unsuccessful uploads
+        },
+        () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                window.document.body.innerHTML = `<a href="${downloadURL}">${downloadURL}</a>`;
+            });
+        }
+    );
+});
